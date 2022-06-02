35f5dc64d770daacec2cfb6b706f7dc68b31fe32
unknown <feather>
2022-06-02 14:28:26 +0800

    evaluator

--- my_player.cpp
+++ my_player.cpp
@@ -19,6 +19,9 @@ class Point{
         Point operator+(const Point &rhs) const;
         Point operator-(const Point &rhs) const;
         bool operator<(const Point &rhs) const;
+
+        friend std::ostream& operator<<(std::ostream& os, const Point& p);
+
         int x;
         int y;
     private:
@@ -43,6 +46,11 @@ bool Point::operator<(const Point &rhs) const{
         return y < rhs.y;
     }
     return x < rhs.x;
+}
+
+std::ostream& operator<<(std::ostream& os, const Point& p){
+    os << '(' << p.x << ", " << p.y << ')';
+    return os;
 }
 
 // ----- State Tree ----- //
@@ -83,6 +91,7 @@ class ChessBoard{
         void delete_piece(int x, int y);
         bool is_valid(int x, int y) const;
         bool is_empty(int x, int y) const;
+        void print() const;
         std::vector<int> &operator[](int i);
 
         friend class DecisionMaker;
@@ -137,6 +146,52 @@ std::vector<int> &ChessBoard::operator[](int i){
     return board[i];
 }
 
+void ChessBoard::print() const{
+    std::cout << "---- Chess Board ----" << std::endl;
+    std::cout << "  ";
+    for(int i = 0; i < SIZE; i++){
+        std::cout << i%10 << ' ';
+    }
+    std::cout << std::endl;
+    for(int x = 0; x < SIZE; x++){
+        std::cout << x%10 << ' ';
+        for(int y = 0; y < SIZE; y++){
+            if(board[x][y] == 0){
+                std::cout << '.' << ' ';
+            }
+            else{
+                std::cout << board[x][y] << ' ';
+            }
+        }
+        std::cout << std::endl;
+    }
+}
+
+// ----- Evaluator ----- //
+
+class Evaluator{
+    //Get a map state and output its score
+    public:
+        Evaluator() {};
+        Evaluator(std::vector<std::vector<int>> &board);
+        float evaluate();
+    private:
+        void get_lines(const std::vector<std::vector<int>> &board);
+        int evaluate_line(std::vector<int> &line);
+        std::vector<std::vector<int>> horizontal_lines = std::vector<std::vector<int>>(15); //for lines "-"
+        std::vector<std::vector<int>> vertical_lines = std::vector<std::vector<int>>(15);   //for lines "|"
+        std::vector<std::vector<int>> up_right_lines = std::vector<std::vector<int>>(30);   //for lines "/"
+        std::vector<std::vector<int>> down_right_lines = std::vector<std::vector<int>>(30); //for lines "\"
+};
+
+Evaluator::Evaluator(std::vector<std::vector<int>> &board){
+
+}
+
+float Evaluator::evaluate(){
+    return (float)(rand()%1000000);
+}
+
 // ----- Decision maker ----- //
 
 class DecisionMaker{
@@ -145,6 +200,8 @@ class DecisionMaker{
         DecisionMaker(char **argv);
         ~DecisionMaker();
         void find_next_step();
+        void print_possible_steps() const;
+        void print_tree(StateTreeNode &node) const;
     private:
         void create_tree(StateTreeNode &node, int depth, int curr_player);
         void get_all_possible_steps();
@@ -176,6 +233,9 @@ DecisionMaker::DecisionMaker(char **argv){
     root = StateTreeNode(player);
     board = ChessBoard(SIZE, fin);
     init_directions();
+
+    std::cout << "Initail board" << std::endl;
+    board.print();
 }
 
 DecisionMaker::~DecisionMaker(){
@@ -186,18 +246,45 @@ DecisionMaker::~DecisionMaker(){
 void DecisionMaker::find_next_step(){
     //create tree
     get_all_possible_steps();
+    print_possible_steps();  
     create_tree(root, 1, player);
+    //print_tree(root);
 
     //use alpha-beta pruning to get next step
     float final_value = alpha_beta_pruning(root, 1, -std::numeric_limits<float>::max(), std::numeric_limits<float>::max(), true);
+    std::cout << "Final_value : " << final_value << std::endl;
+    float max_value = -std::numeric_limits<float>::max();
     for(auto child:root.childs){
-        if(child.value == final_value){
+        std::cout << "child value : " << child.value << " x : " << child.placement.x << " y : " << child.placement.y << std::endl;
+        max_value = std::max(child.value, max_value);
+        //std::cout << max_value << " | " << child.placement.x << ' ' << child.placement.y << std::endl;
+        if(max_value == child.value){
             fout << child.placement.x << ' ' << child.placement.y << std::endl;
         }
     }
 }
 
+void DecisionMaker::print_possible_steps() const{
+    std::cout << "----possble next steps----" << std::endl;
+    for(auto step : possible_step_set){
+        std::cout << step << ' ';
+    }
+    std::cout << std::endl;
+}
+
+void DecisionMaker::print_tree(StateTreeNode &node) const{
+    std::cout << "New Level" << std::endl;
+    for(auto child:node.childs){
+        std::cout << child.placement << ' ';
+    }
+    std::cout << std::endl;
+    for(auto child:node.childs){
+        print_tree(child);
+    }
+}
+
 void DecisionMaker::create_tree(StateTreeNode &node, int depth, int curr_player){
+    //std::cout << "depth: " << depth << " node : " << node.placement << std::endl;
     if(depth >= DEPTH)return;
     int next_player = (curr_player == 1) ? 2:1;
     //create childs
@@ -206,7 +293,7 @@ void DecisionMaker::create_tree(StateTreeNode &node, int depth, int curr_player)
     }
 
     //go through all the childs to let them generate there own childs
-    for(auto child:node.childs){
+    for(auto &child:node.childs){
         std::set<Point> temp;
         
         //update board
@@ -221,6 +308,7 @@ void DecisionMaker::create_tree(StateTreeNode &node, int depth, int curr_player)
                 temp.insert(possible_point);
             }
         }
+        possible_step_set.erase(child.placement);
 
         //let child generate it's own childs
         create_tree(child, depth+1, next_player);
@@ -232,6 +320,7 @@ void DecisionMaker::create_tree(StateTreeNode &node, int depth, int curr_player)
         for(auto point:temp){
             possible_step_set.erase(point);
         }
+        possible_step_set.insert(child.placement);
     }
 }
 
@@ -261,13 +350,16 @@ void DecisionMaker::get_all_possible_steps(){
 
 float DecisionMaker::alpha_beta_pruning(StateTreeNode &node, int depth, float alpha, float beta, bool is_player){
     if(depth >= DEPTH){
-        Evaluator evaluator(board.board);
-        return evaluator.evaluate();
+        Evaluator evaluator = Evaluator(board.board);
+        node.value = evaluator.evaluate();
+        return node.value;
     }
     if(is_player){
         float value = -std::numeric_limits<float>::max();
-        for(auto child:node.childs){
+        for(auto &child:node.childs){
+            board.add_piece(child.placement, DecisionMaker::player);
             value = std::max(value, alpha_beta_pruning(child, depth+1, alpha, beta, false));
+            board.delete_piece(child.placement);
             alpha = std::max(alpha, value);
             if(alpha >= beta){
                 //beta will have the memory of all the child form the node parents(siblings)
@@ -282,8 +374,10 @@ float DecisionMaker::alpha_beta_pruning(StateTreeNode &node, int depth, float al
     else{
         //for enemies turn the smaller the points the better
         float value = std::numeric_limits<float>::max();
-        for(auto child:node.childs){
+        for(auto &child:node.childs){
+            board.add_piece(child.placement, enemy);
             value = std::min(value, alpha_beta_pruning(child, depth+1, alpha, beta, true));
+            board.delete_piece(child.placement);
             beta = std::min(beta, value);
             if(beta <= alpha){
                 //alpha will have the memory of the nodes siblings
@@ -328,27 +422,14 @@ void DecisionMaker::init_directions(){
     directions.push_back(Point(2, -2));
 }
 
-// ----- Evaluator ----- //
-
-class Evaluator{
-    //Get a map state and output its score
-    public:
-        Evaluator() {};
-        Evaluator(const std::vector<std::vector<int>> &board);
-        float evaluate();
-    private:
-        void get_lines(const std::vector<std::vector<int>> &board);
-        int evaluate_line(std::vector<int> &line);
-        std::vector<std::vector<int>> horizontal_lines = std::vector<std::vector<int>>(15); //for lines "-"
-        std::vector<std::vector<int>> vertical_lines = std::vector<std::vector<int>>(15);   //for lines "|"
-        std::vector<std::vector<int>> up_right_lines = std::vector<std::vector<int>>(30);   //for lines "/"
-        std::vector<std::vector<int>> down_right_lines = std::vector<std::vector<int>>(30); //for lines "\"
-};
 
 // ----- Main Function ----- //
 
 int main(int, char** argv) {
+    std::cout << "in program" << std::endl;
     DecisionMaker decision_maker(argv);
+    std::cout << "decision_maker built" << std::endl;
     decision_maker.find_next_step();
+    std::cout << "finish findng next step" << std::endl;
     return 0;
 }
